a9ffaafedbe3ac7596a18c20207383f257d99cea
Vanya Mahlinets <moraxus09@gmail.com>
2018-01-03 16:48:45 +0400

    renamed task

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('sprite', function () {
 		.pipe(gulp.dest('build/img'));
 })
 
-gulp.task('fileInclude', function() {
+gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(fileInclude({
       indent: true,
